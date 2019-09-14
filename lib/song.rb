4ec6f9bb54ec5bcb71aa6f950cb42a362bd148c6
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-14 21:35:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,49 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.detect{|song_name| song_name.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+    
+  def self.alphabetical
+    @@all.sort_by{|w| w.name}
+  end
+  
+  def self.new_from_filename(name)
+    
+  end
+  
+  def self.create_from_filename(name)
+    
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
   def self.all
     @@all
   end
