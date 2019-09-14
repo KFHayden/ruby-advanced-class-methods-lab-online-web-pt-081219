d8fa360e6da95b595b0f3b5ea0afe3532eac4433
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-14 21:44:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,9 @@ class Song
   end
   
   def self.new_from_filename(name)
-    song = self.new
+    song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split (" - ")[0])
+    song.artist_name = (name.split(" - ")[0])
     song
   end
   
